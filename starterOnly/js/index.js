4b1b41243a47d03145912041a75716047245f99f
JerryCow33 <liangelus33@gmail.com>
2023-03-21 19:02:42 +0100

    Begin creating javascript functions

--- starterOnly/js/index.js
+++ starterOnly/js/index.js
@@ -1,15 +1,24 @@
 import { editNav } from './modules/navigation.js'
-import { launchModal, closeModal } from './modules/modal.js'
+import { launchModal, closeModal, submitForm } from './modules/modal.js'
 
 // DOM Elements
 const modalBtn = document.querySelectorAll('.modal-btn')
 const formData = document.querySelectorAll('.formData')
 const toggleNavBtn = document.getElementById('togglenav')
 const closeBtn = document.querySelectorAll('.close');
+const form = document.querySelector('form');
 
 // Show / hide navigation menubars (xs only)
 toggleNavBtn.addEventListener('click', editNav);
 
 // launch modal event
 modalBtn.forEach(btn => btn.addEventListener('click', launchModal))
+
+// close modal event
 closeBtn.forEach(btn => btn.addEventListener('click', closeModal));
+
+// submit form event
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    submitForm();
+});
--- starterOnly/js/modules/modal.js
+++ starterOnly/js/modules/modal.js
@@ -1,4 +1,9 @@
 const modalbg = document.querySelector('.bground');
+const prenom = document.getElementById('first');
+const nom = document.getElementById('last');
+const email = document.getElementById('email');
+const location = document.querySelector('input[name=location]');
+const cg = document.getElementById('checkbox1');
 
 // launch modal form
 export function launchModal () {
@@ -8,4 +13,14 @@ export function launchModal () {
 // close modal form
 export function closeModal () {
     modalbg.style.display = 'none';
+}
+
+// submit form
+export function submitForm () {
+    validate();
+}
+
+
+function validate() {
+    // verify first name
 }
